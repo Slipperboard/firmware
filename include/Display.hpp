e91b4db5fc1ba88a9ce75a003a02fb57dbb8793e
Fadi Hanna Al-Kass <Alkass@users.noreply.github.com>
2025-07-07 13:14:32 -0700

    Remove circular display support

--- include/Display.hpp
+++ include/Display.hpp
@@ -19,23 +19,17 @@ class Display : public Peripheral
     public:
     // Construct a rectangular display
     explicit Display(Dimensions dims);
-    // Construct a circular display
-    explicit Display(int radius);
     ~Display() override;
 
     int getWidth() const;
     int getHeight() const;
-    int getRadius() const;
-    bool isCircular() const;
 
     // Draw a series of bytes at the given coordinate
     virtual void drawBytes(Point pos, const unsigned char *data, std::size_t length) = 0;
 
     protected:
-    bool circular;
     int width;
     int height;
-    int radius;
 };
 
 #endif // DISPLAY_HPP
