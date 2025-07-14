fe9d32abb06e307d947ff8283754a3941fc2e29f
Fadi Hanna Al-Kass <Alkass@users.noreply.github.com>
2025-07-14 09:00:45 -0700

    Add complete operator new/delete overloads

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,8 +1,13 @@
 #define CATCH_CONFIG_RUNNER
+#include <new>
 #include "MemoryTracker.hpp"
 #include "catch_amalgamated.hpp"
 
 void* operator new(std::size_t size)
+{
+    return trackAlloc(size);
+}
+void* operator new(std::size_t size, const std::nothrow_t&) noexcept
 {
     return trackAlloc(size);
 }
@@ -14,7 +19,19 @@ void operator delete(void* ptr, std::size_t) noexcept
 {
     trackFree(ptr);
 }
+void operator delete(void* ptr, const std::nothrow_t&) noexcept
+{
+    trackFree(ptr);
+}
+void operator delete(void* ptr, std::size_t, const std::nothrow_t&) noexcept
+{
+    trackFree(ptr);
+}
 void* operator new[](std::size_t size)
+{
+    return trackAlloc(size);
+}
+void* operator new[](std::size_t size, const std::nothrow_t&) noexcept
 {
     return trackAlloc(size);
 }
@@ -23,6 +40,14 @@ void operator delete[](void* ptr) noexcept
     trackFree(ptr);
 }
 void operator delete[](void* ptr, std::size_t) noexcept
+{
+    trackFree(ptr);
+}
+void operator delete[](void* ptr, const std::nothrow_t&) noexcept
+{
+    trackFree(ptr);
+}
+void operator delete[](void* ptr, std::size_t, const std::nothrow_t&) noexcept
 {
     trackFree(ptr);
 }
