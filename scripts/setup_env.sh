8c77ae22780611b914bbaef7d8b392cb2a64faae
Fadi Hanna Al-Kass <Alkass@users.noreply.github.com>
2025-07-21 20:29:23 -0500

    FIX: clarify env setup

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -15,5 +15,4 @@ case "$(uname)" in
 esac
 
 "$script_dir/install_uv.sh"
-"$script_dir/install_platformio.sh"
-"$script_dir/install_cpplint.sh"
+uv pip install --system -r "$script_dir/../requirements.txt"
